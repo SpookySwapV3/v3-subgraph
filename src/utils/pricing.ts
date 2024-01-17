27646ed6afa21fc4ed10d6fb39af9d4ef1ec446d
RitzyDev <RitzyDevBox@proton.me>
2024-01-16 19:00:13 -0500

    updating pricing to use the WBTT Tokens and pools

--- src/utils/pricing.ts
+++ src/utils/pricing.ts
@@ -4,42 +4,34 @@ import { Bundle, Pool, Token } from './../types/schema'
 import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 import { exponentToBigDecimal, safeDiv } from '../utils/index'
 
-const WETH_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
-const USDC_WETH_03_POOL = '0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8'
+const WNATIVE_ADDRESS = '0x23181F21DEa5936e24163FFABa4Ea3B316B57f3C'
+const PRIMARY_STABLE_WNATIVE_POOL = '0x1d163F8aBA2A8c8cCBFEBea30B93473d7B234E0F' // USDT_T WBTT 03
+
 
 // token where amounts should contribute to tracked volume and liquidity
 // usually tokens that many tokens are paired with s
 export let WHITELIST_TOKENS: string[] = [
-  WETH_ADDRESS, // WETH
-  '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI
-  '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
-  '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
-  '0x0000000000085d4780b73119b644ae5ecd22b376', // TUSD
-  '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', // WBTC
-  '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643', // cDAI
-  '0x39aa39c021dfbae8fac545936693ac917d5e7563', // cUSDC
-  '0x86fadb80d8d2cff3c3680819e4da99c10232ba0f', // EBASE
-  '0x57ab1ec28d129707052df4df418d58a2d46d5f51', // sUSD
-  '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2', // MKR
-  '0xc00e94cb662c3520282e6f5717214004a7f26888', // COMP
-  '0x514910771af9ca656af840dff83e8264ecf986ca', // LINK
-  '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f', // SNX
-  '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e', // YFI
-  '0x111111111117dc0aa78b770fa6a738034120c302', // 1INCH
-  '0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8', // yCurv
-  '0x956f47f50a910163d8bf957cf5846d573e7f87ca', // FEI
-  '0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0', // MATIC
-  '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9', // AAVE
-  '0xfe2e637202056d30016725477c5da089ab0a043a' // sETH2
+  WNATIVE_ADDRESS, // WETH
+  '0xAD9A21FF0c9d854cA8C1360AF28D4fcbDaC53B4F', // FTM_e
+  '0x43559B1786C06d6B826e3cf9AA667eD8840f9106', // ADA_b
+  '0x1249C65AfB11D179FFB3CE7D4eEDd1D9b98AD006', // ETH
+  '0x17F235FD5974318E4E2a5e37919a209f7c37A6d1', // USDD_t
+  '0xdB28719F7f938507dBfe4f0eAe55668903D34a15', // USDT_t
+  '0xE887512ab8BC60BcC9224e1c3b5Be68E26048B8B', // USDT_e
+  '0xAE17940943BA9440540940DB0F1877f101D39e8b', // USDC_e
+  '0xCa424b845497f7204D9301bd13Ff87C0E2e86FCF', // USDC_b
+  '0x9888221fE6B5A2ad4cE7266c7826D2AD74D40CcF', // WBTC__e
+  '0xfd3b093aB6bD4F40810f19e5fF822ac8Cc7e3184', // LINK_e
+  '0xEdf53026aeA60f8F75FcA25f8830b7e2d6200662', // TRX
+  '0x185a4091027E2dB459a2433F85f894dC3013aeB5', // BNB
 ]
 
 let STABLE_COINS: string[] = [
-  '0x6b175474e89094c44da98b954eedeac495271d0f',
-  '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-  '0xdac17f958d2ee523a2206206994597c13d831ec7',
-  '0x0000000000085d4780b73119b644ae5ecd22b376',
-  '0x956f47f50a910163d8bf957cf5846d573e7f87ca',
-  '0x4dd28568d05f09b02220b09c2cb307bfd837cb95'
+  '0x17F235FD5974318E4E2a5e37919a209f7c37A6d1', // USDD_t
+  '0xdB28719F7f938507dBfe4f0eAe55668903D34a15', // USDT_t
+  '0xE887512ab8BC60BcC9224e1c3b5Be68E26048B8B', // USDT_e
+  '0xAE17940943BA9440540940DB0F1877f101D39e8b', // USDC_e
+  '0xCa424b845497f7204D9301bd13Ff87C0E2e86FCF', // USDC_b
 ]
 
 let MINIMUM_ETH_LOCKED = BigDecimal.fromString('60')
@@ -59,7 +51,7 @@ export function sqrtPriceX96ToTokenPrices(sqrtPriceX96: BigInt, token0: Token, t
 
 export function getEthPriceInUSD(): BigDecimal {
   // fetch eth prices for each stablecoin
-  let usdcPool = Pool.load(USDC_WETH_03_POOL) // dai is token0
+  let usdcPool = Pool.load(PRIMARY_STABLE_WNATIVE_POOL) // dai is token0
   if (usdcPool !== null) {
     return usdcPool.token0Price
   } else {
@@ -72,7 +64,7 @@ export function getEthPriceInUSD(): BigDecimal {
  * @todo update to be derived ETH (add stablecoin estimates)
  **/
 export function findEthPerToken(token: Token): BigDecimal {
-  if (token.id == WETH_ADDRESS) {
+  if (token.id == WNATIVE_ADDRESS) {
     return ONE_BD
   }
   let whiteList = token.whitelistPools
