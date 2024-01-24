fb0e088b631a35446d1e86e7a18c9770903fc8bb
RitzyDev <RitzyDevBox@proton.me>
2024-01-23 19:36:06 -0500

    adding support for EON

--- src/utils/staticTokenDefinition.ts
+++ src/utils/staticTokenDefinition.ts
@@ -19,30 +19,64 @@ export class StaticTokenDefinition {
     this.decimals = decimals
   }
 
+
   // Get all tokens with a static defintion
   static getStaticDefinitions(): Array<StaticTokenDefinition> {
-
-    // Add DGD
     return [
       new StaticTokenDefinition(
         Address.fromString(WNATIVE_ADDRESS),
-        'WFTM',
-        'Wrapped Bittorrent',
+        'WZEN',
+        'Wrapped Zen',
         BigInt.fromI32(18)
       ),
       new StaticTokenDefinition(
-        Address.fromString('0x28a92dde19D9989F39A49905d7C9C2FAc7799bDf'),
-        'USDC_LZ',
-        'USD Coin (Layer Zero)',
+        Address.fromString('0x1d7fb99AED3C365B4DEf061B7978CE5055Dfc1e7'),
+        'lzWBTC',
+        'LayerZero WBTC',
+        BigInt.fromI32(8)
+      ),
+      new StaticTokenDefinition(
+        Address.fromString('0x2c2E0B0c643aB9ad03adBe9140627A645E99E054'),
+        'lzETH',
+        'LayerZero ETH',
+        BigInt.fromI32(18)
+      ),
+      new StaticTokenDefinition(
+        Address.fromString('0x6318374DFb468113E06d3463ec5Ed0B6Ae0F0982'),
+        'lzAVAX',
+        'LayerZero AVAX',
         BigInt.fromI32(6)
       ),
       new StaticTokenDefinition(
-        Address.fromString('0x1B6382DBDEa11d97f24495C9A90b7c88469134a4'),
-        'USDC_AXL',
-        'USD Coin (Axelar)',
-        BigInt.fromI32(8)
-      )
-  
+        Address.fromString('0xDF8DBA35962Aa0fAD7ade0Df07501c54Ec7c4A89'),
+        'lzLINK',
+        'LayerZero LINK',
+        BigInt.fromI32(18)
+      ),
+      new StaticTokenDefinition(
+        Address.fromString('0x38C2a6953F86a7453622B1E7103b738239728754'),
+        'lzDAI',
+        'LayerZero DAI',
+        BigInt.fromI32(18)
+      ),
+      new StaticTokenDefinition(
+        Address.fromString('0xCc44eB064CD32AAfEEb2ebb2a47bE0B882383b53'),
+        'lzUSDC',
+        'LayerZero USDC',
+        BigInt.fromI32(6)
+      ),
+      new StaticTokenDefinition(
+        Address.fromString('0xA167bcAb6791304EDa9B636C8beEC75b3D2829E6'),
+        'lzUSDT',
+        'LayerZero USDT',
+        BigInt.fromI32(6)
+      ),
+      new StaticTokenDefinition(
+        Address.fromString('0xCEad8ee30e03aE87E5E709617f7FdF180Eef9973'),
+        'ZUSD',
+        'ZEN USD',
+        BigInt.fromI32(6)
+      ),  
     ]
   }
 
