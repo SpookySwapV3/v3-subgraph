ffe11b3af7b27b47d51687f7fe3c121559293b1f
0xALUCARD <0xALUCARD@protonmail.com>
2024-05-02 15:46:48 +0200

    updates

--- src/utils/staticTokenDefinition.ts
+++ src/utils/staticTokenDefinition.ts
@@ -1,10 +1,5 @@
 import { Address, BigInt } from '@graphprotocol/graph-ts'
-import {
-  Address,
-  BigInt,
-} from "@graphprotocol/graph-ts"
 import { WNATIVE_ADDRESS } from "./pricing"
-  
 
 // Initialize a Token Definition with the attributes
 export class StaticTokenDefinition {
@@ -15,29 +10,27 @@ export class StaticTokenDefinition {
 
   // Get all tokens with a static defintion
   static getStaticDefinitions(): Array<StaticTokenDefinition> {
-
-    // Add DGD
-    return [
-      new StaticTokenDefinition(
-        Address.fromString(WNATIVE_ADDRESS),
-        'WFTM',
-        'Wrapped Fantom',
-        BigInt.fromI32(18)
-      ),
-      new StaticTokenDefinition(
-        Address.fromString('0x28a92dde19d9989f39a49905d7c9c2fac7799bdf'),
-        'USDC_LZ',
-        'USD Coin (Layer Zero)',
-        BigInt.fromI32(6)
-      ),
-      new StaticTokenDefinition(
-        Address.fromString('0x1b6382dbdea11d97f24495c9a90b7c88469134a4'),
-        'USDC_AXL',
-        'USD Coin (Axelar)',
-        BigInt.fromI32(8)
-      )
-  
+    const staticDefinitions: Array<StaticTokenDefinition> = [
+      {
+        address: Address.fromString(WNATIVE_ADDRESS),
+        symbol: 'WFTM',
+        name: 'Wrapped Fantom',
+        decimals: BigInt.fromI32(18),
+      },
+      {
+        address: Address.fromString('0x28a92dde19d9989f39a49905d7c9c2fac7799bdf'),
+        symbol: 'USDC_LZ',
+        name: 'USD Coin (Layer Zero)',
+        decimals: BigInt.fromI32(6),
+      },
+      {
+        address: Address.fromString('0x1b6382dbdea11d97f24495c9a90b7c88469134a4'),
+        symbol: 'USDC_AXL',
+        name: 'USD Coin (Axelar)',
+        decimals: BigInt.fromI32(8),
+      },
     ]
+    return staticDefinitions
   }
 
   // Helper for hardcoded tokens
